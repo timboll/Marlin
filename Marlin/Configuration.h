7c96e318815747e661bf870e66772f3f616fcd9c
timothyb <timothyaboll@gmail.com>
2023-09-14 17:44:22 -0400

    updated z axis steps per unit (from 400 to 800)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1220,7 +1220,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 433.7 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 433.7 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
